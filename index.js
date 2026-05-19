1a5168b8f2cb2bc1fa7c9813abb2dd1e547073e5
jituman01 <naimulislamjitu585@gmail.com>
2026-05-19 17:08:47 +0600

    implement jwks

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const express = require('express')
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 const app = express()
 app.use(cors());
@@ -11,7 +12,13 @@ const port = process.env.PORT || 8080
 
 
 
-const uri = "mongodb+srv://DocAppoint:sQlLQxLQL42IA4nO@cluster0.umcyfpz.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+  
+)
+// console.log(JWKS, "JWKS");
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,16 +29,84 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} | ${req.url}`);
+  next()
+      
+};
+
+const verifyToken = async (req, res, next) => {
+  const { authorization}=req.headers
+  // console.log(req.headers,"from verify token");
+  const token = authorization?.split(" ")[1];
+  console.log(token);
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const JWKS = createRemoteJWKSet(
+      new URL('http://localhost:3000/api/auth/jwks')
+    )
+    const { payload } = await jwtVerify(token, JWKS);
+    // return payload
+    req.user = payload;
+    console.log(req.user);
+    next();
+  }
+  
+  catch (error) {
+    console.error('Token validation failed:', error)
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  
+
+  
+  
+}
+
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+    const db = client.db('docappoint');
+    const appointmentCollection = db.collection('doctor');
+
+    app.get('/appointments', async (req, res) => {
+      const cursor = appointmentCollection.find();
+      const result = await cursor.toArray();
+      // console.log(result);
+      res.send(result);
+    });
+
+    app.get('/appointments/:appointmentId', logger, verifyToken,
+      async (req, res,) => {
+        console.log(req.user, "req");
+        
+      const { appointmentId } = req.params;
+      // console.log(appointmentId);
+      const query ={ _id: new ObjectId(appointmentId) }
+      const result = await appointmentCollection.findOne(query);
+      res.send(result);
+      
+    })
+
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
